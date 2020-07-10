87f28fad118b0500d6b45212d603edd7a6b75f09
Sharon Ho <sharon.cy.ho@gmail.com>
2020-07-10 19:41:25 -0400

    Render Sharon class in main function

--- Source/main.cpp
+++ Source/main.cpp
@@ -27,25 +27,24 @@
 #include <Objects/Shaders.h>
 #include <Objects/Grid.h> //rendered objects
 #include <Objects/Camera.h>
-#include <Objects/LetterL.h>
-#include <Objects/Number9.h>
 #include <Objects/Thomas.h>
 #include <Objects/Melina.h>
+#include <Objects/Sharon.h>
 
-int main(int argc, char*argv[])
+int main(int argc, char* argv[])
 {
     // Initialize GLFW and OpenGL version
     glfwInit();
 
 #if defined(PLATFORM_OSX)	
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2); 
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #else
-	// On windows, we set OpenGL version to 2.1, to support more hardware
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    // On windows, we set OpenGL version to 2.1, to support more hardware
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 #endif
 
     // Create Window and rendering context using GLFW, resolution is 1024x768
@@ -73,9 +72,9 @@ int main(int argc, char*argv[])
     Shaders shaders;
     int shaderProgram = shaders.compileAndLinkShaders();
 
-	// Create Camera Object
-	Camera camera(window);
-    
+    // Create Camera Object
+    Camera camera(window);
+
     // Define and upload geometry to the GPU here ...
     Grid objGrid;
     int gridVAO = objGrid.createGridVAO();
@@ -84,18 +83,17 @@ int main(int argc, char*argv[])
     Cube objCube;
     int cubeVAO = objCube.createCubeVAO();
     glm::mat4 worldMatrix = mat4(1.0f);
-	
-	//LetterO letter;
-    LetterL letter;
-	Number9 num9;
+
+    // Models
     Thomas Model1;
-	Melina Model2;
+    Melina Model2;
+    Sharon Model3;
 
     // Entering Main Loop
-    while(!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(window))
     {
-		// Enable z-buffer
-		glEnable(GL_DEPTH_TEST);
+        // Enable z-buffer
+        glEnable(GL_DEPTH_TEST);
 
         // Each frame, reset color of each pixel to glClearColor
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -104,55 +102,50 @@ int main(int argc, char*argv[])
         glm::mat4 Projection = glm::perspective(glm::radians(45.0f),  // field of view in degrees
             1024.0f / 768.0f,     // aspect ratio
             0.01f, 100.0f);      // near and far (near > 0)
-    	
-        
-		GLuint projectionMatrixLocation = glGetUniformLocation(shaderProgram, "projectionMatrix");
-		glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, &Projection[0][0]);
-		
-    	
+
+        GLuint projectionMatrixLocation = glGetUniformLocation(shaderProgram, "projectionMatrix");
+        glUniformMatrix4fv(projectionMatrixLocation, 1, GL_FALSE, &Projection[0][0]);
+
         //glPolygonMode(GL_FRONT, GL_LINE);
-		// Draw grid and axis
+        // Draw grid and axis
         glUseProgram(shaderProgram);
         glBindVertexArray(gridVAO);
         glDrawArrays(GL_LINES, 0, objGrid.gridToPrint); // 3 vertices, starting at index 0
         glBindVertexArray(axisVAO);
         glDrawArrays(GL_LINES, 0, objGrid.axisToPrint);
-        
-		// Draw letters
+
+        // Draw letters
         GLuint worldMatrixLocation = glGetUniformLocation(shaderProgram, "worldMatrix"); //linking with shader
         glBindVertexArray(cubeVAO);
-
-		//letter.draw(worldMatrixLocation);
-        //letter.concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.0f, 0.0f, 0.0f)));
-        //num9.draw(worldMatrixLocation);
         Model1.draw(worldMatrixLocation);
-		Model2.draw(worldMatrixLocation);
+        Model2.draw(worldMatrixLocation);
+        Model3.draw(worldMatrixLocation);
         glBindVertexArray(0);
 
         glUniformMatrix4fv(worldMatrixLocation, 1, GL_FALSE, &worldMatrix[0][0]); //*Important: setting worldmatrix back to normal so other stuff doesn't get scaled down
 
-    	// Camera frame timing
-		camera.handleFrameData();
+        // Camera frame timing
+        camera.handleFrameData();
 
-		// Set up Camera
-		glm::mat4 viewMatrix = glm::lookAt(camera.cameraPos, // position
+        // Set up Camera
+        glm::mat4 viewMatrix = glm::lookAt(camera.cameraPos, // position
             vec3(0.0f, 0.0f, 0.0f), // front camera.cameraPos + camera.cameraFront
-			camera.cameraUp);  // up
-		GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "viewMatrix");
-		glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, &viewMatrix[0][0]);
+            camera.cameraUp);  // up
+        GLuint viewMatrixLocation = glGetUniformLocation(shaderProgram, "viewMatrix");
+        glUniformMatrix4fv(viewMatrixLocation, 1, GL_FALSE, &viewMatrix[0][0]);
 
         // End frame
         glfwSwapBuffers(window);
-        
+
         // Detect inputs
         glfwPollEvents();
 
-		// Handle inputs
-		camera.handleKeyboardInputs();
+        // Handle inputs
+        camera.handleKeyboardInputs();
     }
-    
+
     // Shutdown GLFW
     glfwTerminate();
-    
-	return 0;
+
+    return 0;
 }
