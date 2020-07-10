c0a687a69eb177a3a271bfefb86633342b663aa1
Sharon Ho <sharon.cy.ho@gmail.com>
2020-07-10 19:40:21 -0400

    Adjusted models to be above grid

--- VS2017/Objects/Melina.cpp
+++ VS2017/Objects/Melina.cpp
@@ -9,7 +9,7 @@ void Melina::setup() {
 	number.concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.1f, 0.0f, 0.0f)));
 	letter.concatWorldMatrix(glm::translate(mat4(1.0f), vec3(-0.1f, 0.0f, 0.0f)));
 
-	concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.75f, 0.05f, -0.75f))); //Translate to top left corner
+	concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.75f, 0.01f, -0.75f))); // Translate to top right corner
 }
 
 mat4 Melina::getWorldMatrix() {
--- VS2017/Objects/Thomas.cpp
+++ VS2017/Objects/Thomas.cpp
@@ -5,11 +5,11 @@ Thomas::Thomas() {
 	setup();
 }
 
-void Thomas::setup() { //create letter O
-	number.concatWorldMatrix( glm::translate(mat4(1.0f), vec3(0.1f, 0.0f, 0.0f)) );
-	letter.concatWorldMatrix( glm::translate(mat4(1.0f), vec3(-0.1f, 0.0f, 0.0f)) );
+void Thomas::setup() {
+	number.concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.1f, 0.0f, 0.0f)));
+	letter.concatWorldMatrix(glm::translate(mat4(1.0f), vec3(-0.1f, 0.0f, 0.0f)));
 
-	concatWorldMatrix( glm::translate(mat4(1.0f), vec3(-0.75f, 0.05f, -0.75f)) ); //Translate to top left corner
+	concatWorldMatrix(glm::translate(mat4(1.0f), vec3(-0.75f, 0.01f, -0.75f))); // Translate to top left corner
 }
 
 mat4 Thomas::getWorldMatrix() {
