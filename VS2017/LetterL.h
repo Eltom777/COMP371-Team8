0d3f2f30d6f38ce3d737cd8c8b6f215591263b69
Lerihan <phan.melina@gmail.com>
2020-07-10 04:28:58 -0400

    Added new class LetterL

---
+++ VS2017/LetterL.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "AlphaNumeric.h"
+
+static const int NUMOFCUBES = 4;
+
+class LetterL : public AlphaNumeric
+{
+public:
+	LetterL();
+	~LetterL();
+	void setup();
+	void concatWorldMatrix(mat4 mat);
+	mat4 getWorldMatrix();
+	void draw(GLuint worldMatrixLocation);
+
+private:
+	Cube components[NUMOFCUBES];
+	mat4 worldMatrix;
+};
+
