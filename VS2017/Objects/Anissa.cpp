9851fb9ca06e135e0cf74b14a0ab1d6050ca068a
gdzooks <issa4044@gmail.com>
2020-07-10 20:45:23 -0400

    Move 3 in Anissa model.

--- VS2017/Objects/Anissa.cpp
+++ VS2017/Objects/Anissa.cpp
@@ -7,7 +7,7 @@ Anissa::Anissa() {
 
 void Anissa::setup() {
 	letter.concatWorldMatrix(glm::translate(mat4(1.0f), vec3(-0.1f, 0.0f, 0.0f)));
-	number.concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.01f, 0.001f, 0.0f)));
+	number.concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.01f, 0.005f, 0.0f)));
 
 	concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.75f, 0.01f, 0.75)));
 }
