9f6b8c938f0b2371c856b209a8c2f0cdd07426ff
Sharon Ho <sharon.cy.ho@gmail.com>
2020-07-12 20:51:20 -0400

    Modified position of Keven model on the grid

--- VS2017/Objects/Keven.cpp
+++ VS2017/Objects/Keven.cpp
@@ -9,7 +9,7 @@ void Keven::setup() {
 	number.concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.1f, 0.0f, 0.0f)));
 	letter.concatWorldMatrix(glm::translate(mat4(1.0f), vec3(-0.1f, 0.0f, 0.0f)));
 
-	concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.0f, 0.01f, -0.75f))); // Translate to top left corner
+	concatWorldMatrix(glm::translate(mat4(1.0f), vec3(0.0f, 0.01f, -0.05f))); // Translate to top left corner
 }
 
 mat4 Keven::getWorldMatrix() {
