2562cbbe5e705d5cc491434975aca4aab91022dd
Eltom777 <lthomas.flynn@gmail.com>
2020-07-21 23:39:58 -0400

    modified AlphaNumeric to have a dynamic array of cubes. In addition, the transformation were modified.

--- VS2017/Objects/AlphaNumeric.cpp
+++ VS2017/Objects/AlphaNumeric.cpp
@@ -4,17 +4,49 @@ mat4 AlphaNumeric::getModelMatrix() {
 	return modelMatrix;
 }
 
-void AlphaNumeric::translate(mat4 t) {
-	translationMatrix = t * translationMatrix;
-	updateModelMatrix();
+AlphaNumeric::AlphaNumeric(int numOfCubes) {
+	numberOfCubes = numOfCubes;
+	components = new Cube[numberOfCubes];
 }
 
-void AlphaNumeric::scale(mat4 t) {
-	scalingMatrix = t * scalingMatrix;
-	updateModelMatrix();
+void AlphaNumeric::translateModel(mat4 t)
+{
+	modelMatrix = t * modelMatrix;
+	for (int i = 0; i < numberOfCubes; i++) {
+		components[i].updateTranslation(t);
+	}
 }
 
-void AlphaNumeric::rotate(mat4 t) {
-	rotationMatrix = t * rotationMatrix;
-	updateModelMatrix();
+void AlphaNumeric::scaleModel(mat4 s)
+{
+	//Place back to origin
+	vec3 translationComponent = vec3(modelMatrix[3][0], modelMatrix[3][1], modelMatrix[3][2]);
+	mat4 tempworldMatrix = translate(mat4(1.0), translationComponent * -1.0f); //place back to origin
+	translateModel(tempworldMatrix);
+
+	modelMatrix = s * modelMatrix;
+	translateModel(s);
+
+	//Place back to original spot
+	tempworldMatrix = translate(mat4(1.0), translationComponent);
+	translateModel(tempworldMatrix);
+}
+
+void AlphaNumeric::rotateModel(mat4 r)
+{
+	//Place back to origin
+	vec3 translationComponent = vec3(modelMatrix[3][0], modelMatrix[3][1], modelMatrix[3][2]);
+	mat4 tempworldMatrix = translate(mat4(1.0), translationComponent * -1.0f); //place back to origin
+	translateModel(tempworldMatrix);
+
+	modelMatrix = r * modelMatrix;
+	translateModel(r);
+
+	//Place back to original spot
+	tempworldMatrix = translate(mat4(1.0), translationComponent);
+	translateModel(tempworldMatrix);
+}
+
+AlphaNumeric::~AlphaNumeric() {
+	delete[] components;
 }
--- VS2017/Objects/AlphaNumeric.h
+++ VS2017/Objects/AlphaNumeric.h
@@ -4,16 +4,20 @@
 class AlphaNumeric
 {
 public:
+	AlphaNumeric(int numOfCubes);
 	mat4 getModelMatrix();
 	virtual void updateModelMatrix() = 0;
-	void translate(mat4 mat);
-	void scale(mat4 mat);
-	void rotate(mat4 mat);
+	void translateModel(mat4 t);
+	void scaleModel(mat4 s);
+	void rotateModel(mat4 r);
 	virtual void draw(GLuint modelMatrixLocation) = 0;
+	~AlphaNumeric();
 
 protected:
 	mat4 modelMatrix = mat4(1.0f);
 	mat4 translationMatrix = mat4(1.0f);
 	mat4 scalingMatrix = mat4(1.0f);
 	mat4 rotationMatrix = mat4(1.0f);
+	int numberOfCubes;
+	Cube* components;
 };
