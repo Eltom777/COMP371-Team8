d61488b1cf38730b1aae326bfa19efea68811487
Eltom777 <lthomas.flynn@gmail.com>
2020-07-23 22:26:45 -0400

    Removed texturedGrid and placed everything inside Grid

--- Source/main.cpp
+++ Source/main.cpp
@@ -28,7 +28,6 @@
 #include <Objects/Sharon.h>
 #include <Objects/Anissa.h>
 #include <Objects/Keven.h>
-#include <Objects/texturedGrid.h>
 
 // which model we are currently looking at (0, 1, 2, 3, 4)
 // if -1, then we are not looking at any models
@@ -89,7 +88,7 @@ void renderGridAxisCube(int shaderProgram, int* VAO, Grid objGrid) {
 	// Draw grid and axis
 	glUseProgram(shaderProgram);
 	glBindVertexArray(VAO[1]);
-	glDrawArrays(GL_LINES, 0, objGrid.gridToPrint); // 3 vertices, starting at index 0
+	//glDrawArrays(GL_LINES, 0, objGrid.gridToPrint); // 3 vertices, starting at index 0
 	glBindVertexArray(VAO[2]);
 	glDrawArrays(GL_LINES, 0, objGrid.axisToPrint);
 	glBindVertexArray(VAO[0]);
@@ -502,9 +501,9 @@ int main(int argc, char* argv[])
 	// Define and upload geometry to the GPU here ...
 	Grid objGrid;
 	Cube objCube;
-	texturedGrid objtexture;
-	int textureID = objtexture.loadTexture();
-	int textureVAO = objtexture.createtextureGridVAO();
+	//texturedGrid objtexture;
+	int textureID = objGrid.loadTexture();
+	int textureVAO = objGrid.createtextureGridVAO();
 	int* VAO = createCubeGridVAO(objCube, objGrid);
 
 	// Entering Main Loop
@@ -528,7 +527,7 @@ int main(int argc, char* argv[])
 		
 		
 		glBindVertexArray(textureVAO);
-		glDrawElements(GL_TRIANGLES, objtexture.gridToPrint, GL_UNSIGNED_INT, nullptr);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		// Render grid and axis and cube
 		renderGridAxisCube(texturedShaderProgram, VAO, objGrid);
--- VS2017/Objects/Grid.h
+++ VS2017/Objects/Grid.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <glm/glm.hpp>
 #include <GL/glew.h>
+#include <iostream>
 
 using namespace glm;
 
@@ -10,13 +11,27 @@ static const int SIZEOFARRAY = (COLUMNS + COLUMNS + 2) * 4; //Total # of vertice
 static const float AXISLENGTH = static_cast<float>(10) / static_cast <float>(COLUMNS); // 5 * normalise -> normalise = 2 / Columns
 
 class Grid {
+	const char* filename = "../Assets/Textures/brick.jpg";
 
+	const struct TexturedColoredVertex
+	{
+		TexturedColoredVertex(vec3 _position, vec3 _color, vec2 _uv)
+			: position(_position), color(_color), uv(_uv) {}
 
+		vec3 position;
+		vec3 color;
+		vec2 uv;
+	};
 private:
     static const vec3 axis[];
+	static const TexturedColoredVertex textureGrid[];
+	static const int indices[];
 	vec3 grid[SIZEOFARRAY];
 	GLuint gridVAO;
 	GLuint axisVAO;
+	GLuint textureGridVAO;
+	GLuint textureID;
+	mat4 modelMatrix = mat4(1.0f);
 public:
 	const int gridToPrint = SIZEOFARRAY >> 1;
 	const int axisToPrint = 6;
@@ -26,5 +41,8 @@ public:
 	int createAxisVAO();
 	int createGridVAO();
 	int getNumberOfColumns();
+	int createtextureGridVAO();
+	GLuint loadTexture();
+	//void draw(GLuint worldMatrixLocation, bool isTexture);
 };
 
