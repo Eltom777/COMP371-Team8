45c9d6bcc7e75fdeba5b53028805257460dd8355
Eltom777 <lthomas.flynn@gmail.com>
2020-07-23 22:26:45 -0400

    Removed texturedGrid and placed everything inside Grid

--- VS2017/Objects/Grid.h
+++ VS2017/Objects/Grid.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <glm/glm.hpp>
 #include <GL/glew.h>
+#include <iostream>
 
 using namespace glm;
 
@@ -10,11 +11,27 @@ const int SIZEOFARRAY = (COLUMNS + COLUMNS + 2) * 4; //Total # of vertices -> (n
 const float AXISLENGTH = static_cast<float>(10) / static_cast <float>(COLUMNS); // 5 * normalise -> normalise = 2 / Columns
 
 class Grid {
+	const char* filename = "../Assets/Textures/brick.jpg";
+
+	const struct TexturedColoredVertex
+	{
+		TexturedColoredVertex(vec3 _position, vec3 _color, vec2 _uv)
+			: position(_position), color(_color), uv(_uv) {}
+
+		vec3 position;
+		vec3 color;
+		vec2 uv;
+	};
 private:
     static const vec3 axis[];
+	static const TexturedColoredVertex textureGrid[];
+	static const int indices[];
 	vec3 grid[SIZEOFARRAY];
 	GLuint gridVAO;
 	GLuint axisVAO;
+	GLuint textureGridVAO;
+	GLuint textureID;
+	mat4 modelMatrix = mat4(1.0f);
 public:
 	const int gridToPrint = SIZEOFARRAY >> 1;
 	const int axisToPrint = 6;
@@ -23,5 +40,8 @@ public:
 	void generateGrid();
 	int createAxisVAO();
 	int createGridVAO();
+	int createtextureGridVAO();
+	GLuint loadTexture();
+	//void draw(GLuint worldMatrixLocation, bool isTexture);
 };
 
