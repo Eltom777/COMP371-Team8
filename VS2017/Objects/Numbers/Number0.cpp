51bfa0b5ef3d2de68690429421f06ba4d3932d05
gdzooks <issa4044@gmail.com>
2020-07-27 18:57:12 -0400

    Split Keven model into top & bottom components.

--- VS2017/Objects/Numbers/Number0.cpp
+++ VS2017/Objects/Numbers/Number0.cpp
@@ -1,7 +1,8 @@
 #include "Number0.h"
 
-Number0::Number0(): AlphaNumeric(NUMOFCUBES) {
-	numberOfCubes = NUMOFCUBES;
+Number0::Number0(): AlphaNumeric(NUMOFTOPCUBES, NUMOFBOTCUBES) {
+	numberOfTopCubes = NUMOFTOPCUBES;
+	numberOfBotCubes = NUMOFBOTCUBES;
 	setup();
 }
 
@@ -10,28 +11,47 @@ void Number0::setup() {
 	glm::vec3 rotationAxis(0.0f, 0.0f, 1.0f);
 	
 	// hardcoded relative positions
-	components[0].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 6.5f, 2.0f)));
-	components[0].updateTranslation(glm::translate(mat4(1.0f), vec3(0.035f, 0.0f, 0.0f)));
+	bottomComponents[0].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 2.0f, 2.0f)));
+	bottomComponents[0].updateTranslation(glm::translate(mat4(1.0f), vec3(-0.035f, -0.04f, 0.0f))); // left (bottom)
 
-	components[1].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 6.5f, 2.0f)));
-	components[1].updateTranslation(glm::translate(mat4(1.0f), vec3(-0.035f, 0.0f, 0.0f)));
+	bottomComponents[1].updateScale(glm::scale(mat4(1.0f), vec3(3.0f, 2.0f, 2.0f)));
+	bottomComponents[1].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, -0.06f, 0.0f))); // bottom
+	
+	bottomComponents[2].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 2.0f, 2.0f)));
+	bottomComponents[2].updateTranslation(glm::translate(mat4(1.0f), vec3(0.035f, -0.04f, 0.0f))); // right (bottom)
 
-	components[2].updateScale(glm::scale(mat4(1.0f), vec3(3.0f, 2.0f, 2.0f)));
-	components[2].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, 0.06f, 0.0f)));
+	topComponents[0].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 4.5f, 2.0f)));
+	topComponents[0].updateTranslation(glm::translate(mat4(1.0f), vec3(0.035f, 0.015f, 0.0f))); // right (top)
 
-	components[3].updateScale(glm::scale(mat4(1.0f), vec3(3.0f, 2.0f, 2.0f)));
-	components[3].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, -0.06f, 0.0f)));
+	topComponents[1].updateScale(glm::scale(mat4(1.0f), vec3(3.0f, 2.0f, 2.0f)));
+	topComponents[1].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, 0.06f, 0.0f))); // top
+
+	topComponents[2].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 4.5f, 2.0f)));
+	topComponents[2].updateTranslation(glm::translate(mat4(1.0f), vec3(-0.035f, 0.015f, 0.0f))); // left (top)
 
 	// Cross in the number
-	components[4].updateScale(glm::scale(mat4(1.0f), vec3(3.0f, 1.0f, 2.0f)));
-	components[4].updateRotation(glm::rotate(mat4(1.0f), 45.0f, rotationAxis));
+	topComponents[3].updateScale(glm::scale(mat4(1.0f), vec3(3.0f, 1.0f, 2.0f)));
+	topComponents[3].updateRotation(glm::rotate(mat4(1.0f), 45.0f, rotationAxis));
+
+	// setup hierarchy
+	bottomComponents[0].updateChild(&bottomComponents[1]);
+	bottomComponents[1].updateChild(&bottomComponents[2]);
+	bottomComponents[2].updateChild(&topComponents[0]);
+	updateBase(&bottomComponents[0]);
+	topComponents[0].updateChild(&topComponents[1]);
+	topComponents[1].updateChild(&topComponents[2]);
+	topComponents[2].updateChild(&topComponents[3]);
 
 	// set letter slightly above grid
-	for (int i = 0; i < NUMOFCUBES; i++) {
+	traverse(glm::translate(mat4(1.0f), vec3(0.0f, 0.07f, 0.0f)), 0);
+
+	/*for (int i = 0; i < NUMOFCUBES; i++) {
 		components[i].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, 0.07f, 0.0f)));
-	}
+	}*/
 }
 
 Number0::~Number0() {
-	delete[] components;
+	//delete[] components;
+	delete[] topComponents;
+	delete[] bottomComponents;
 }
