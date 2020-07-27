557a6239fd2265a26a1f39a19263846cd1cb73fd
gdzooks <issa4044@gmail.com>
2020-07-27 17:58:21 -0400

    Split letter O into top & bottom components.

--- VS2017/Objects/Letters/LetterO.h
+++ VS2017/Objects/Letters/LetterO.h
@@ -3,7 +3,9 @@
 
 class LetterO : public AlphaNumeric
 {
-	static const int NUMOFCUBES = 4;
+	static const int NUMOFCUBES = 6;
+	static const int NUMOFTOPCUBES = 3;
+	static const int NUMOFBOTCUBES = 3;
 
 public:
 	LetterO();
