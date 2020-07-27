2c622a640b877276cda2b0181d852254b3e7e64e
Sharon Ho <sharon.cy.ho@gmail.com>
2020-07-27 19:10:17 -0400

    Split Sharon model

--- VS2017/Objects/Letters/LetterA.cpp
+++ VS2017/Objects/Letters/LetterA.cpp
@@ -1,30 +1,51 @@
 #include "LetterA.h"
 
-LetterA::LetterA() : AlphaNumeric(NUMOFCUBES) {
 LetterA::LetterA() : AlphaNumeric(NUMOFCUBES, true) {
-LetterA::LetterA() : AlphaNumeric(NUMOFCUBES) {
-	numberOfCubes = NUMOFCUBES;
+	setup();
+}
+
+LetterA::LetterA() : AlphaNumeric(NUMOFTOPCUBES, NUMOFBOTCUBES) {
+	numberOfTopCubes = NUMOFTOPCUBES;
+	numberOfBotCubes = NUMOFBOTCUBES;
 	setup();
 }
 
 void LetterA::setup() { //create letter A
 	// hardcoded relative positions
-	components[0].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 7.5f, 2.0f)));
-	components[0].updateTranslation(glm::translate(mat4(1.0f), vec3(0.05f, 0.0f, 0.0f)));// right edge
+	topComponents[0].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 2.75f, 2.0f)));
+	topComponents[0].updateTranslation(glm::translate(mat4(1.0f), vec3(0.05f, 0.0045f, 0.0f)));// right edge top
 
-	components[1].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 7.5f, 2.0f)));
-	components[1].updateTranslation(glm::translate(mat4(1.0f), vec3(-0.05f, 0.0f, 0.0f)));// left edge
+	topComponents[1].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 2.75f, 2.0f)));
+	topComponents[1].updateTranslation(glm::translate(mat4(1.0f), vec3(-0.05f, 0.0045f, 0.0f)));// left edge top
 
-	components[2].updateScale(glm::scale(mat4(1.0f), vec3(3.0f, 1.5f, 2.0f)));
-	components[2].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, 0.06f, 0.0f)));// top edge
+	topComponents[2].updateScale(glm::scale(mat4(1.0f), vec3(7.0f, 1.5f, 2.0f)));
+	topComponents[2].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, 0.045f, 0.0f)));// top edge
 
-	components[3].updateScale(glm::scale(mat4(1.0f), vec3(3.0f, 1.0f, 2.0f)));
-	components[3].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, 0.005f, 0.0f)));
+	topComponents[3].updateScale(glm::scale(mat4(1.0f), vec3(3.0f, 1.5f, 2.0f))); // middle edge
+	topComponents[3].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, -0.01f, 0.0f)));
+
+	bottomComponents[0].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 2.75f, 2.0f)));
+	bottomComponents[0].updateTranslation(glm::translate(mat4(1.0f), vec3(0.05f, -0.045f, 0.0f)));// right edge bottom
+
+	bottomComponents[1].updateScale(glm::scale(mat4(1.0f), vec3(2.0f, 2.75f, 2.0f)));
+	bottomComponents[1].updateTranslation(glm::translate(mat4(1.0f), vec3(-0.05f, -0.045f, 0.0f)));// left edge bottom
+
+	bottomComponents[1].updateChild(&bottomComponents[0]); //base
+	bottomComponents[0].updateChild(&topComponents[0]);
+	updateBase(&bottomComponents[1]);
+
+	topComponents[0].updateChild(&topComponents[1]);
+	topComponents[1].updateChild(&topComponents[2]);
+	topComponents[2].updateChild(&topComponents[3]);
 
 	// set letter slightly above grid
-	for (int i = 0; i < NUMOFCUBES; i++) {
-		components[i].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, 0.065f, 0.0f)));
-	}
+	traverse(glm::translate(mat4(1.0f), vec3(0.0f, 0.065f, 0.0f)), 0);
+	
+	//bottomComponents[0].updateChild(NULL);
+	
+	//for (int i = 0; i < NUMOFCUBES; i++) {
+	//	components[i].updateTranslation(glm::translate(mat4(1.0f), vec3(0.0f, 0.065f, 0.0f)));
+	//}
 }
 
 void LetterA::updateModelMatrix() {
@@ -35,5 +56,6 @@ void LetterA::updateModelMatrix() {
 }
 
 LetterA::~LetterA() {
-	delete[] components;
+	delete[] topComponents;
+	delete[] bottomComponents;
 }
--- VS2017/Objects/Numbers/Number4.h
+++ VS2017/Objects/Numbers/Number4.h
@@ -3,7 +3,9 @@
 
 class Number4 : public AlphaNumeric
 {
-	static const int NUMOFCUBES = 3;
+	static const int NUMOFCUBES = 4;
+	static const int NUMOFTOPCUBES = 3;
+	static const int NUMOFBOTCUBES = 1;
 
 public:
 	Number4();
