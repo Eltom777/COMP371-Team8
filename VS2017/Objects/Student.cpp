03522d4cc1f218d2d0723f3efdc2d7b372ae9c71
Eltom777 <lthomas.flynn@gmail.com>
2020-07-27 01:17:17 -0400

    fixed rotation and scaling issue

--- VS2017/Objects/Student.cpp
+++ VS2017/Objects/Student.cpp
@@ -20,8 +20,6 @@ void Student::translate(mat4 t) {
 
 void Student::scale(mat4 s) {
 
-	modelMatrix = s * modelMatrix;
-
 	//Place back to origin
 	vec3 translationComponent = vec3(modelMatrix[3][0], modelMatrix[3][1], modelMatrix[3][2]);
 	mat4 tempworldMatrix = glm::translate(mat4(1.0), translationComponent * -1.0f); //place back to origin
@@ -44,13 +42,6 @@ void Student::scale(mat4 s) {
 // NOTE TO SELF (Melina): remove all worldMatrixLocations params after finished testing :|
 void Student::rotate(mat4 r) {
 	
-	//TESTING
-	//components[1]->rotateModel(r, worldMatrixLocation);
-	////components[1]->rotateModel(r, worldMatrixLocation);
-
-	//modelMatrix = r * modelMatrix;
-
-
 	//Place back to origin
 	vec3 translationComponent = vec3(modelMatrix[3][0], modelMatrix[3][1], modelMatrix[3][2]);
 	mat4 tempworldMatrix = glm::translate(mat4(1.0), translationComponent * -1.0f); //place back to origin
@@ -61,8 +52,6 @@ void Student::rotate(mat4 r) {
 	for (AlphaNumeric* component : components) {
 		component->translateModel(r);
 	}
-
-	modelMatrix = r * modelMatrix;
 
 	//Place back to original spot
 	tempworldMatrix = glm::translate(mat4(1.0), translationComponent);
@@ -80,6 +69,7 @@ void Student::draw(Shader* shaderProgram, const bool isTexture) {
 void Student::create() {
 	components[0]->create();
 	components[1]->create();
+}
 
 void Student::randomLocation(float x, float z)
 {
